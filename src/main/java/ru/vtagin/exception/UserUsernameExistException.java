d14c09a87d4352e1fc658f44739053f5a53c0254
Dmitriy <freejase@bk.ru>
2022-05-23 16:23:21 +0300

    Initial commit

---
+++ src/main/java/ru/vtagin/exception/UserUsernameExistException.java
@@ -0,0 +1,9 @@
+package ru.vtagin.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+
+public class UserUsernameExistException extends DataIntegrityViolationException {
+    public UserUsernameExistException(String msg) {
+        super(msg);
+    }
+}
